ffaa421fc83bb330809fe86e74ba26c3edbfc3c6
“kulkarohan” <53792428+kulkarohan@users.noreply.github.com>
2021-09-18 22:41:37 -0400

    update morty token transfer

--- contracts/MortyVault.sol
+++ contracts/MortyVault.sol
@@ -8,11 +8,10 @@ import '@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgra
 import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
 
 contract MortyVault is ERC20Upgradeable, ERC721HolderUpgradeable {
-    /// ============ Token Information ============
+    /// ============ Collateral Token Information ============
 
     /// @notice the ERC721 token address of the vault's token
     address public token;
-
     /// @notice the ERC721 token ID of the vault's token
     uint256 public id;
 
@@ -20,21 +19,27 @@ contract MortyVault is ERC20Upgradeable, ERC721HolderUpgradeable {
 
     /// @notice the address who initially deposited the NFT
     address public curator;
-
     /// @notice the amount of mortys in the vault
     uint256 public balance;
-
     /// @notice a boolean to indicate if the vault has closed
     bool public vaultClosed;
+    ///
+    string name;
+    ///
+    string symbol;
+    ///
+    address public buyerLP;
 
     /// ============ Events ============
 
-    event CollateralSwap(
+    event CollateralSwapped(
         address curator,
         address tokenAddress,
         uint256 previousTokenId,
         uint256 newTokenId
     );
+
+    event MartingaleStarted(address curator, address token, string name, string symbol);
 
     /// ============ Vault Initializer ============
 
@@ -53,18 +58,26 @@ contract MortyVault is ERC20Upgradeable, ERC721HolderUpgradeable {
         token = _token;
         id = _id;
         curator = _curator;
+        name = _name;
+        symbol = _symbol;
 
-        // send curator 100 tokens
-        _mint(_curator, 100000000000000000000);
+        // initialize vault with a balance of 100 tokens
+        _mint(address(this), 100000000000000000000);
+
+        balance = 100000000000000000000;
     }
 
     /// ============ Curator Functions ============
 
     function start() external {
         require(msg.sender == curator);
+        require(balanceOf(address(this)) == 100000000000000000000);
 
-        // transfer 50 tokens to vault
-        this.transferFrom(curator, payable(address(this)), 50000000000000000000);
+        // approve transfer of 50 tokens
+        this.approve(msg.sender, 50000000000000000000);
+        // transfer 50 tokens to curator
+        transferFrom(address(this), curator, 50000000000000000000);
+
         balance = 50000000000000000000;
 
         // trigger Martingale RNG
@@ -79,7 +92,7 @@ contract MortyVault is ERC20Upgradeable, ERC721HolderUpgradeable {
         // Send previous token back to user
         IERC721(token).safeTransferFrom(address(this), msg.sender, id);
 
-        emit CollateralSwap(msg.sender, token, id, _tokenId);
+        emit CollateralSwapped(msg.sender, token, id, _tokenId);
 
         // Update token id
         id = _tokenId;
